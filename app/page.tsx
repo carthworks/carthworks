39d26a3df768499e00c76a5029396f7cf5f1ced5
karthikeyant <tkarthikeyan@pitowings.com>
2026-02-09 16:50:19 +0530

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Skills from '@/components/sections/Skills';
 import Projects from '@/components/sections/Projects';
 import Contact from '@/components/sections/Contact';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
     return (
@@ -14,6 +15,7 @@ export default function Home() {
             <Projects />
             <Contact />
             <Footer />
+            <Analytics />
         </main>
     );
 }
